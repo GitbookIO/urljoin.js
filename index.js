a6de6bc10e2a9d5e014f6ddf2ab38dbd9d866202
Aaron O'Mullan <aaron.omullan@gmail.com>
2015-07-07 16:44:29 -0700

    Handle multiple args

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 var urlResolve = require('url').resolve;
 
-// urlJoin joins two urls
+// _urlJoin joins two urls
 // By resolving a url relative to the root url
 // this works nicely with extra slashes in places
-function urlJoin(rootUrl, url) {
+function _urlJoin(rootUrl, url) {
     // Normalize then resolve URLs
     return urlResolve(
         // Ensure trailing slash
-        rootUrl.replace(/\/+$/, '') + '/',
+        (rootUrl || '').replace(/\/+$/, '') + '/',
         // Trim preceding slash
         (url || '').replace(/^\/+/, '')
     );
+}
+
+// urlJoin joins all arguments as URLs
+function urlJoin() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    return args.slice(1)
+    .reduce(function(accu, x) {
+        return _urlJoin(accu, x);
+    }, args[0] || '/');
+
 }
 
 module.exports = urlJoin;
